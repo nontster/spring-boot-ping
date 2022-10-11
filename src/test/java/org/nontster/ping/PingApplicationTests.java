6bb03e4f188c102d7e6c0310f207083c60d9ba34
Nont Banditwong <nontster@gmail.com>
2022-10-11 21:40:00 +0700

    Add Unit Test for Liveness and Readiness probe

--- src/test/java/org/nontster/ping/PingApplicationTests.java
+++ src/test/java/org/nontster/ping/PingApplicationTests.java
@@ -3,6 +3,9 @@ package org.nontster.ping;
 import org.junit.jupiter.api.Test;
 import org.nontster.ping.controller.PingController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.availability.ApplicationAvailability;
+import org.springframework.boot.availability.LivenessState;
+import org.springframework.boot.availability.ReadinessState;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -21,12 +24,26 @@ class PingApplicationTests {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
+	@Autowired
+	private ApplicationAvailability applicationAvailability;
+
 	@Test
 	void pingShouldReturnPong (){
 		final String url = String.format("http://localhost:%d/ping", port);
 		assertThat(this.restTemplate.getForObject(url,String.class)).contains("pong");
 	}
 
+	@Test
+	void livenessStateisCorrect(){
+		assertThat(applicationAvailability.getLivenessState())
+				.isEqualTo(LivenessState.CORRECT);
+	}
+
+	@Test
+	void readinessStateAcceptingTraffic(){
+		assertThat(applicationAvailability.getReadinessState())
+				.isEqualTo(ReadinessState.ACCEPTING_TRAFFIC);
+	}
 	@Test
 	void contextLoads() {
 		assertThat(controller).isNotNull();
